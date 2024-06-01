dde3ea270822bd825a48d14337ee0fcc9f7be10a
JAVIER <javiertrigueropomares@gmail.com>
2024-06-01 19:56:34 +0200

    JTP - termino proyecto usuario

--- 16-proyecto/index.js
+++ 16-proyecto/index.js
@@ -1,9 +1,20 @@
+let serialize = target => Array.from(target.elements).reduce((acc,el)=>{
+    if(!el.name) return acc;
+    acc[el.name] =el.value;
+    return acc;
+},{});
+
 class User{
+    
     static #url="https://jsonplaceholder.typicode.com/users";
     static #users=[];
     static #ul=document.createElement('ul');
-    
-
+    static #form= document.createElement('form');
+    static #initialValues = {name:'',email:''};
+    constructor(data){
+        this.name=data.name;
+        this.email=data.email;
+    }
     static async getAll(){
         try {
             const response = await fetch(this.#url);   
@@ -24,15 +35,82 @@ class User{
     static render(){
         let users=this.#users;
         users.forEach(u=> this.#ul.appendChild(this.renderUser(u)));
-
         return this.#ul;
+    }
+
+    static onSubmit(e){
+        e.preventDefault();
+        let data=serialize(e.target);
+        let user= new User(data);
+        const errors= user.validate();
+        if (Object.keys(errors).length>0) {
+            console.log(this);
+            this.#form.innerHTML= this.formHTML({data,errors});
+            return;
+        }
+
+        user.save();
+    }
+    save(){
+        return User.save(this);
+    }
+    static async save(user){
+        try {
+            const response= await fetch(this.#url,{
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body:JSON.stringify(user)
+                
+            });
+            const data= await response.json();
+            this.#users.unshift(data);
+            this.#ul.prepend(this.renderUser(data));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    validate(){
+        let errors={};
+        if(!this.name){
+            errors.name="Nombre es obligatorio"
+        }
+        if(!this.email){
+            errors.email="Email es obligatorio"
+        }
+        return errors;
+    }
+    static formHTML({data,errors}){
+        return `
+        <div>
+        <label for=""> Nombre: </label>
+            <input name="name" value="${data.name}"></input>
+            ${errors.name || ''}
+        </div>
+        
+        <div>
+        
+        <label for="">Email:</label>
+            <input name="email" value="${data.name}"></input>
+            ${errors.email || ''}
+    </div>
+        <input type="submit" value="Enviar"></input>
+        `;
+    }
+    static renderForm(){
+
+        this.#form.onsubmit=this.onSubmit.bind(this);
+        this.#form.innerHTML=this.formHTML({data:this.#initialValues,errors:{}});
+        return this.#form;
     }
 }
 async function main(){
+
         const users= await User.getAll();
         const template=User.render();
-        console.log(template);
+        const form= User.renderForm();
+
         document.body.insertAdjacentElement('afterbegin',template);
+        document.body.insertAdjacentElement('afterbegin',form);
 }
 
 main();
